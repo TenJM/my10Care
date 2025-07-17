ea92e1c7abbf615dd5b528555968a6833833abf8
TenJM <thendralraj19@gmail.com>
2025-07-17 13:06:29 +0530

    Changes made at 11:35

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import (
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
+import os
 
 # Initialize app
 app = Flask(__name__)
@@ -144,8 +145,11 @@ def get_products():
 # ---------------------------
 # Run the server
 # ---------------------------
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
